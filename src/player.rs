26a3afe6868a9060ef8c311f4179b997f6ec30be
Hilmar Wiegand <hilmar.wiegand@flameshome.net>
2017-03-08 09:11:00 +0100

    :wrench: change number format for player values

--- src/player.rs
+++ src/player.rs
@@ -2,10 +2,10 @@ use piston_window::{rectangle, Context, G2d};
 
 use super::camera::Camera;
 
-const PLAYER_WIDTH: u32 = 36;
-const PLAYER_HEIGHT: u32 = 50;
+const PLAYER_WIDTH: f64 = 36.0;
+const PLAYER_HEIGHT: f64 = 50.0;
 
-const PLAYER_SPEED: f64 = 20.0;
+const PLAYER_SPEED: f64 = 200.0;
 
 pub struct Player {
     pos: [f64; 2],
@@ -23,15 +23,15 @@ impl Player {
     }
 
     pub fn update(&mut self, dt: f64) {
-        self.pos[0] += self.vel[0] * PLAYER_SPEED * dt * 10.0;
-        self.pos[1] += self.vel[1] * PLAYER_SPEED * dt * 10.0;
+        self.pos[0] += self.vel[0] * PLAYER_SPEED * dt;
+        self.pos[1] += self.vel[1] * PLAYER_SPEED * dt;
     }
 
     pub fn draw(&self, c: &Context, g: &mut G2d, cam: &Camera) {
         rectangle(self.color,
-                  [self.pos[0] - cam.pos[0] - (0.5 * PLAYER_WIDTH as f64),
-                   self.pos[1] - cam.pos[1] - (0.5 * PLAYER_HEIGHT as f64),
-                   PLAYER_WIDTH as f64, PLAYER_HEIGHT as f64],
+                  [self.pos[0] - cam.pos[0] - (0.5 * PLAYER_WIDTH),
+                   self.pos[1] - cam.pos[1] - (0.5 * PLAYER_HEIGHT),
+                   PLAYER_WIDTH, PLAYER_HEIGHT],
                   c.transform, g);
     }
 
